07569fd00398b781c3be7a703d43b8d21fe05589
Anderson Danilo <contato@andersondanilo.com>
2014-10-17 22:11:18 -0300

    alterações

---
+++ ServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tee\User;
+
+use Tee\User\Widgets\MainMenu;
+use Widget;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider {
+
+    public function register()
+    {
+        
+    }
+}
