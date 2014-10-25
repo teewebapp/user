984fb71b12b05ace6110497957bb5d215dbdeb5d
Anderson Danilo <contato@andersondanilo.com>
2014-10-25 15:31:49 -0200

    Correção

--- ServiceProvider.php
+++ ServiceProvider.php
@@ -5,10 +5,13 @@ namespace Tee\User;
 use Tee\User\Widgets\MainMenu;
 use Tee\System\Widget;
 
+use App;
+
 class ServiceProvider extends \Illuminate\Support\ServiceProvider {
 
     public function register()
     {
-        
+        App::register('Cviebrock\EloquentSluggable\SluggableServiceProvider');
+        App::register('Codesleeve\LaravelStapler\LaravelStaplerServiceProvider');
     }
 }
